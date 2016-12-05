2c3cb348b7476169dd9571e4d86adb1e1489fc2b
daw <daw@PCDAW>
2016-12-05 17:33:58 +0100

    Versió 0.2 del projecte CeskAlex

--- src/ceskalex/CeskAlex.java
+++ src/ceskalex/CeskAlex.java
@@ -16,6 +16,8 @@ public class CeskAlex {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo.");
+        System.out.println("Adiòs Mundo.");
     }
     
 }
